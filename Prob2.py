08285c42765610a3a1ad63c978bcb741458c56d5
suzanne1207 <sgunderson@willamette.edu>
2024-10-13 09:18:36 -0700

    Update Prob2.py

--- Prob2.py
+++ Prob2.py
@@ -1,7 +1,7 @@
 ########################################
-# Name:
-# Collaborators:
-# Estimated time spent (hrs):
+# Name: Suzanne Gunderson
+# Collaborators: N/A
+# Estimated time spent (hrs): 3
 ########################################
 
 from pgl import GWindow, GRect
@@ -20,21 +20,19 @@ def draw_pyramid():
     gw = GWindow(WIDTH, HEIGHT)
 
     # You got it from here
+    draw_rows()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+def draw_rows():
+    gw = GWindow(WIDTH,HEIGHT)
+    for i in range(BRICKS_IN_BASE): # loops for each row
+        BRICKS_IN_ROW = BRICKS_IN_BASE - i # each row has one less brick each time
+        X = (WIDTH - BRICKS_IN_ROW * BRICK_WIDTH) //2 # x is the length of the row divided by 2 so it's centered
+        Y = (HEIGHT - (BRICKS_IN_BASE * BRICK_HEIGHT)) //2 + (BRICK_HEIGHT * BRICKS_IN_BASE) - (BRICK_HEIGHT * (i+1)) # y takes the height of the entire pyramid from the height and divides it in two to center it, then adds the rows yet to be added
+        for i in range(BRICKS_IN_ROW):
+            b = GRect(X, Y, BRICK_WIDTH, BRICK_HEIGHT) # draws a brick
+            gw.add(b) # adds the brick
+            X = X + BRICK_WIDTH # x changes for each brick in the row
+            Y = Y # y stays the same for each brick in the row
 
 
 
